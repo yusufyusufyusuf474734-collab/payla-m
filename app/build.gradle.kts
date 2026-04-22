0de6ea64613150f945f91a6399e218aa371fb901
Sinanooq <terkedilmisduygular@hotmail.com>
2026-04-22 13:34:41 +0300

    fix: use MaterialComponents theme, add material dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,5 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.11.0")
 }
